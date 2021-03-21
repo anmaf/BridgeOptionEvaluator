90b62845af20be4497eea3edfabda4768a73338a
Adrian Foltyn <adrianfoltyn@Adrians-MacBook-Air.local>
2021-03-21 13:19:30 +0100

    move layout element sizes to config

--- src/config.py
+++ src/config.py
@@ -1,10 +1,14 @@
 from eval_funs import bridge_notation
 
+gui_theme = 'Dark Green 5'
+card_list_size = (4,13)
+output_window_size = (40,15)
+default_button_size = (4,2)
 card_list = list(range(2, 15))
 card_type_list = ['small', 'reg', '-']
 our_hands_list = ['S', 'N', '-']
-fontA = ("Helvetica", 8)
-fontAb = ("Helvetica-Bold", 8)
+fontA = ("Helvetica", 11)
+fontAb = ("Helvetica-Bold", 11)
 button_colors = ('yellow', 'darkgreen')
 echo_in_console = False
 
--- src/layout.py
+++ src/layout.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
 from config import *
 
-sg.theme('Dark Green 5')
+sg.theme(gui_theme)
 col1 = [[sg.Text('S hand', font=fontAb)], \
         [sg.Listbox(values = BN_form_card_list, no_scrollbar = True, select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, \
-                    font=fontA, size=(5,13), key = 'S_hand')]]
+                    font=fontA, size=card_list_size, key = 'S_hand')]]
 col2 = [[sg.Text('N hand', font=fontAb)], \
         [sg.Listbox(values = BN_form_card_list, no_scrollbar = True, select_mode=sg.LISTBOX_SELECT_MODE_MULTIPLE, \
-                    font=fontA, size=(5,13), key = 'N_hand')]]
+                    font=fontA, size=card_list_size, key = 'N_hand')]]
 col3 = [[sg.Text('Hand', font=fontAb)], [sg.OptionMenu(values = our_hands_list, default_value='-', key = 'hand1')], \
         [sg.OptionMenu(values = our_hands_list, default_value='-', key = 'hand2')],\
         [sg.OptionMenu(values = our_hands_list, default_value='-', key = 'hand3')]]
@@ -20,7 +20,8 @@ col5 = [[sg.Text('Card 3', font=fontAb)], [sg.OptionMenu(values = plan_list, def
 col6 = [[sg.Text('Finesse', font=fontAb)],[sg.Checkbox('', key = 'cb1')], [sg.Checkbox('', key = 'cb2')],\
         [sg.Checkbox('', key = 'cb3')]]
 
-layout = [[sg.Column(col1, element_justification='c'), sg.VSeparator(), sg.Column(col2, element_justification='c')],\
+layout = [[sg.Column(col1, element_justification='c'), sg.VSeparator(color='grey'), \
+           sg.Column(col2, element_justification='c')],\
           [sg.Column(col3, element_justification='c'), sg.Column(col4, element_justification='c'),\
            sg.Column(col5, element_justification='c'), sg.Column(col6, element_justification='c')], \
           [sg.Checkbox('Show tricks', font=fontA, default=True, key = 'cb_tricks'), \
@@ -28,7 +29,7 @@ layout = [[sg.Column(col1, element_justification='c'), sg.VSeparator(), sg.Colum
           [sg.Button('Submit', button_color=button_colors, font = fontAb, key = 'Submit'), \
             sg.Button('Clear', button_color=button_colors, font = fontAb, key = 'Clear'), \
            sg.Button('Clear output', button_color=button_colors, font=fontAb, key='ClearOut')],\
-          [sg.Output(size=(40,10), echo_stdout_stderr = echo_in_console,  font=fontA, key='OUTPUT')]]
+          [sg.Output(size=output_window_size, echo_stdout_stderr = echo_in_console,  font=fontA, key='OUTPUT')]]
 
 window = sg.Window(
-    'Bridge Option Evaluator', layout, default_button_element_size=(4, 2), font=fontA)
+    'Bridge Option Evaluator', layout, default_button_element_size=default_button_size, font=fontA)
